aca276f09f8107af32528393a9d33575629626d9
José García <josemgu91@gmail.com>
2015-08-05 15:45:33 -0500

    Agregué comentarios a la clase 2 del módulo 1.

--- m1/c2/MyFirstApplication/app/src/main/java/pe/area51/myfirstapplication/HelloActivity.java
+++ m1/c2/MyFirstApplication/app/src/main/java/pe/area51/myfirstapplication/HelloActivity.java
@@ -10,7 +10,9 @@ public class HelloActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        //Obtenemos el Intent con el que se inició este Activity para luego obtener el String enviado.
         final String name = getIntent().getStringExtra(MainActivity.EXTRA_NAME);
+        //Asignamos la cadena enviada al TextView.
         ((TextView) findViewById(R.id.activity_hello_textview_hello))
                 .setText(getString(R.string.hello, name));
     }
--- m1/c2/MyFirstApplication/app/src/main/java/pe/area51/myfirstapplication/MainActivity.java
+++ m1/c2/MyFirstApplication/app/src/main/java/pe/area51/myfirstapplication/MainActivity.java
@@ -1,53 +1,36 @@
 package pe.area51.myfirstapplication;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    //Utilizaremos este valor para evitar equivocarnos al escribir la clave.
     public static final String EXTRA_NAME = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Establecemos la vista para este Activity.
         setContentView(R.layout.activity_main);
+        //Buscamos el Button y le asignamos un objeto OnClickListener para ejecutar una serie de rutinas cuando lo presionemos.
         findViewById(R.id.activity_main_button_send)
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        final String name = ((EditText)findViewById(R.id.activity_main_edittext_name)).getText().toString();
+                        //Obtenemos el nombre del EditText.
+                        final String name = ((EditText) findViewById(R.id.activity_main_edittext_name)).getText().toString();
+                        //Creamos un Intent para iniciar el Activity "HelloActivity".
                         final Intent intent = new Intent(MainActivity.this, HelloActivity.class);
+                        //Le asignamos el nombre al Intent para poder recuperarlo desde el "HelloActivity".
                         intent.putExtra(EXTRA_NAME, name);
+                        //Iniciamos el Activity "HelloActivity".
                         startActivity(intent);
                     }
                 });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
